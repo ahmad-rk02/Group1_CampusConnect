7e712ecc7b9fe2d4f79963bc9d292d38e34878f2
Anjali_Indrakumar_Rinayat <anjalirinayat52@gmail.com>
2024-09-15 05:55:52 +0530

    added carousel to cse page

--- src/pages/Departments/Cse/CSE.jsx
+++ src/pages/Departments/Cse/CSE.jsx
@@ -1,10 +1,90 @@
 import React from 'react'
 import CSECarousel from './CSECarousel'
+import { Container, Row, Col, Card, ListGroup } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
 
 const CSE = () => {
   return (
     <div>
-      <CSECarousel/>
+      <Container fluid className="p-0 w-100">
+
+        <Row className='head-box-cse'>
+          <Col>
+            <h1 className="text-left">DEPARTMENTS</h1>
+          </Col>
+        </Row>
+
+        <Row>
+
+          <CSECarousel />
+
+          <Row noGutters className="flex-nowrap left-index just overlay-row" >
+            {/* Left Sidebar */}
+            <Col md={2} className='left-sidebar' class="left-sidebar">
+              <Card className="left-nav-cse" class='left-nav-cse'>
+                <ListGroup variant="flush">
+                  <ListGroup.Item className="left-nav-row-01">
+                    <Link
+                      to="/ug"
+                      className={location.pathname === "/cse" ? "active-link" : ""}
+                    >
+                      Computer Science & Engineering
+                    </Link>
+                  </ListGroup.Item>
+                  <ListGroup.Item className="left-nav-row-cse">
+                    <Link
+                      to="/pg"
+                      className={location.pathname === "/pg" ? "active-link" : ""}
+                    >
+                      Electronics & Telecommunication Engineering
+                    </Link>
+                  </ListGroup.Item>
+                  <ListGroup.Item className="left-nav-row-cse">
+                    <Link
+                      to="/phd"
+                      className={location.pathname === "/phd" ? "active-link" : ""}
+                    >
+                      Instrumentation Engineering
+                    </Link>
+                  </ListGroup.Item>
+                  <ListGroup.Item className="left-nav-row-cse">
+                    <Link
+                      to="/phd"
+                      className={location.pathname === "/phd" ? "active-link" : ""}
+                    >
+                      Electrical Engineering
+                    </Link>
+                  </ListGroup.Item>
+                  <ListGroup.Item className="left-nav-row-cse">
+                    <Link
+                      to="/phd"
+                      className={location.pathname === "/phd" ? "active-link" : ""}
+                    >
+                      Mechanical Engineering
+                    </Link>
+                  </ListGroup.Item>
+                  <ListGroup.Item className="left-nav-row-cse">
+                    <Link
+                      to="/phd"
+                      className={location.pathname === "/phd" ? "active-link" : ""}
+                    >
+                      Civil Engineering
+                    </Link>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
+            </Col>
+
+            <Col className='right-content-cse'>
+
+
+            </Col>
+
+          </Row>
+
+        </Row>
+
+      </Container>
     </div>
   )
 }
--- src/pages/Departments/Cse/CSECarousel.jsx
+++ src/pages/Departments/Cse/CSECarousel.jsx
@@ -1,57 +1,111 @@
-import react from 'react';
+import React from 'react';
 import './CSECarousel.css';
 import Carousel from 'react-bootstrap/Carousel';
-// import cseCarousel01 from '../assets/cse-carousel-01.jpg';
-// import cseCarousel02 from '../assets/cse-carousel-02.jpg';
-// import cseCarousel03 from '../assets/cse-carousel-03.jpg';
-// import cseCarousel04 from '../assets/cse-carousel-04.jpg';
-// import cseCarousel05 from '../assets/cse-carousel-05.jpg';
-// import cseCarousel06 from '../assets/cse-carousel-06.jpg';
-// import cseCarousel07 from '../assets/cse-carousel-07.jpg';
+
+// const cseCarousel01 = require('../assets/cse-carousel-01.jpg');
+// const cseCarousel02 = require('../assets/cse-carousel-02.jpg');
+// const cseCarousel03 = require('../assets/cse-carousel-03.jpg');
+// const cseCarousel04 = require('../assets/cse-carousel-04.jpg');
+
+import cseCarousel01 from '../../../assets/cse-carousel-01.jpg';
+import cseCarousel02 from '../../../assets/cse-carousel-02.jpg';
+import cseCarousel03 from '../../../assets/cse-carousel-03.jpg';
+import cseCarousel04 from '../../../assets/cse-carousel-04.jpg';
+import cseCarousel05 from '../../../assets/cse-carousel-05.jpg';
+import cseCarousel06 from '../../../assets/cse-carousel-06.jpg';
+import cseCarousel07 from '../../../assets/cse-carousel-07.jpg';
 
 
-function CSECarousel(){
-    return (
-        <div>
-              <Carousel>
-        <Carousel.Item>
-          <img
-            style={{ height: '80vh', paddingBlock: '0vh' }}
-            className="d-block w-100"
-            src={cseCarousel01}
-            alt="First slide"
-          />
-        </Carousel.Item>
+function CSECarousel() {
+  return (
+    <div>
 
-        <Carousel.Item>
-          <img
-            style={{ height: '80vh' }}
-            className="d-block w-100"
-            src={cseCarousel02}
-            alt="Second slide"
-          />
-        </Carousel.Item>
+       <Carousel interval={2000} className='cse-carousel'>
+            <Carousel.Item>
+              <img
+                style={{ height: '80vh', paddingBlock: '0vh' }}
+                className="d-block w-100"
+                src={cseCarousel01}
+                alt="First slide"
+              />
+            </Carousel.Item>
 
-        <Carousel.Item>
-          <img
-            style={{ height: '80vh' }}
-            className="d-block w-100"
-            src={cseCarousel03}
-            alt="Third slide"
-          />
-        </Carousel.Item>
+            <Carousel.Item>
+              <img
+                style={{ height: '80vh' }}
+                className="d-block w-100"
+                src={cseCarousel02}
+                alt="Second slide"
+              />
+            </Carousel.Item>
 
-        <Carousel.Item>
-          <img
-            style={{ height: '80vh' }}
-            className="d-block w-100"
-            src={cseCarousel04}
-            alt="Fourth slide"
-          />
-        </Carousel.Item>
-      </Carousel>
-        </div>
-    );
+            <Carousel.Item>
+              <img
+                style={{ height: '80vh' }}
+                className="d-block w-100"
+                src={cseCarousel03}
+                alt="Third slide"
+              />
+            </Carousel.Item>
+
+            <Carousel.Item>
+              <img
+                style={{ height: '80vh' }}
+                className="d-block w-100"
+                src={cseCarousel04}
+                alt="Fourth slide"
+              />
+            </Carousel.Item>
+
+            <Carousel.Item>
+              <img
+                style={{ height: '80vh' }}
+                className="d-block w-100"
+                src={cseCarousel05}
+                alt="Fourth slide"
+              />
+            </Carousel.Item>
+
+            <Carousel.Item>
+              <img
+                style={{ height: '80vh' }}
+                className="d-block w-100"
+                src={cseCarousel05}
+                alt="Fourth slide"
+              />
+            </Carousel.Item>
+
+            <Carousel.Item>
+              <img
+                style={{ height: '80vh' }}
+                className="d-block w-100"
+                src={cseCarousel06}
+                alt="Fourth slide"
+              />
+            </Carousel.Item>
+
+            <Carousel.Item>
+              <img
+                style={{ height: '80vh' }}
+                className="d-block w-100"
+                src={cseCarousel06}
+                alt="Fourth slide"
+              />
+            </Carousel.Item>
+
+            <Carousel.Item>
+              <img
+                style={{ height: '80vh' }}
+                className="d-block w-100"
+                src={cseCarousel07}
+                alt="Fourth slide"
+              />
+            </Carousel.Item>
+          </Carousel>
+
+
+    </div>
+  );
 }
 
 export default CSECarousel
