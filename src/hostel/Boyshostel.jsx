e2fabba33868d23716ee84a51a94db5b70ec3ffe
VinniJD <vinnidhanwalkar@gmail.com>
2024-09-23 16:37:30 +0530

    Updated boys hostel page

--- src/hostel/Boyshostel.jsx
+++ src/hostel/Boyshostel.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import './Boyshostel.css';  // Import the CSS file for styling
 
-
-const Boyshostel = () => {
+const Boyshostel = ({ className }) => {
   return (
-    <div>
+    <div className="boys-hostel">
+    <Card.Header className="bg-goldenn">
+        <h4>Committees & Clubs</h4>
+    </Card.Header>
       
     </div>
   )
