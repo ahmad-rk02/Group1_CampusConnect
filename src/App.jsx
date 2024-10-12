5b541a30621f28efbe34cb186337574f2d62e4af
vaishu-dd45 <vaishnvirahangdale123@gmail.com>
2024-10-12 21:46:44 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ import Forgotpassword from './Administration/Forgotpassword';
 import Forgotpassword2 from './Administration/Forgotpassword2';
 import AboutCIIIT from "./CIIITPage/AboutCIIIT";
 import AdmissionForm from "./CIIITPage/AdmissionForm";
-import CentersUnderCIIIT from "./CIIITPage/CentersUnderCIIIT";
+
 import CIIITBrochure from "./CIIITPage/CIIITBrochure";
 import CIIITContact from "./CIIITPage/CIIITContact";
 import CIIITLeaflet from "./CIIITPage/CIIITLeaflet";
@@ -105,7 +105,7 @@ function App() {
                   <Route path="/signupadmin" element={<SignupAdmin />} />
                   <Route path="/aboutCIIIT" element={<AboutCIIIT />} />
                   <Route path="/admissionform" element={<AdmissionForm />} />
-                  <Route path="/centersunderciiit" element={<CentersUnderCIIIT />} />
+                 
                   <Route path="/ciiitbrochure" element={<CIIITBrochure />} />
                   <Route path="/ciiitcontact" element={<CIIITContact />} />
                   <Route path="/ciiitleaflet" element={<CIIITLeaflet />} />
