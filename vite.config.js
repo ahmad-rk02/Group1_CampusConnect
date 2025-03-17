24e5e880ca94781a7c3df3153e3423464ff73c33
Anjali_Indrakumar_Rinayat <anjalirinayat52@gmail.com>
2025-03-17 20:56:54 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.BASE_URL || "/Group1_CampusConnect/", // Use environment variable for flexibility
+  base: "/", // Use environment variable for flexibility
   plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+    open: true
+  },
   resolve: {
     alias: {
       'pg-native': false, // Prevent Vite from bundling 'pg-native'
