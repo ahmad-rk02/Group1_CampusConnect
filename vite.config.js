34b8915811b1eac8eeead9ed44a1c46bac6c0fe2
Anjali_Indrakumar_Rinayat <anjalirinayat52@gmail.com>
2025-03-17 16:24:16 +0530

    netlify file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Group1_CampusConnect/",
+  base: "/Group1_CampusConnect/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      'pg-native': false
+    }
+  },
+  optimizeDeps: {
+    exclude: ['pg-native']
+  },
+  define: {
+    global: {}
+  }
 })
