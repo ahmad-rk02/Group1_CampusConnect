e42a8f94540dd1ae40f46a84f49237b458bf48ba
ahmad-rk02 <razakhanahmad68@gmail.com>
2025-03-19 14:47:41 +0530

    added

--- src/pages/chatbotCTA.jsx
+++ src/pages/chatbotCTA.jsx
@@ -4,7 +4,7 @@ import './chatbotCTA.css';
 function ChatbotCTA() {
     const openChatbot = () => {
         window.open(
-            "https://cdn.botpress.cloud/webchat/v2.2/shareable.html?configUrl=https://files.bpcontent.cloud/2024/10/15/11/20241015114548-QIM5KDRH.json",
+            "https://cdn.botpress.cloud/webchat/v2.3/shareable.html?configUrl=https://files.bpcontent.cloud/2024/10/15/11/20241015114548-QIM5KDRH.json",
             "_blank", 
             "noopener,noreferrer"
         );
