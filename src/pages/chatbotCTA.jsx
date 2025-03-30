4cedc5b7b401c50aaa9661d12bb50666a984be30
ahmad-rk02 <razakhanahmad68@gmail.com>
2025-03-30 15:11:45 +0530

    added

--- src/pages/chatbotCTA.jsx
+++ src/pages/chatbotCTA.jsx
@@ -1,20 +1,35 @@
-import React from 'react';
-import './chatbotCTA.css';
+import React, { useState } from "react";
+import "./chatbotCTA.css";
 
 function ChatbotCTA() {
-    const openChatbot = () => {
-        window.open(
-            "https://cdn.botpress.cloud/webchat/v2.3/shareable.html?configUrl=https://files.bpcontent.cloud/2024/10/15/11/20241015114548-QIM5KDRH.json",
-            "_blank", 
-            "noopener,noreferrer"
-        );
+    const [isChatbotOpen, setIsChatbotOpen] = useState(false);
+
+    const toggleChatbot = () => {
+        setIsChatbotOpen(!isChatbotOpen);
     };
 
     return (
         <div>
-            <div className="chatbot-cta" onClick={openChatbot}>
+            {/* Chatbot Button */}
+            <div className="chatbot-cta" onClick={toggleChatbot}>
                 <i className="fas fa-comments"></i> Chat
             </div>
+
+            {/* Chatbot Popup */}
+            {isChatbotOpen && (
+                <div className="chatbot-container">
+                    <div className="chatbot-header">
+                        <span>Chatbot</span>
+                        <button className="close-btn" onClick={toggleChatbot}>&times;</button>
+                    </div>
+                    <iframe
+                        src="https://cdn.botpress.cloud/webchat/v2.3/shareable.html?configUrl=https://files.bpcontent.cloud/2024/10/15/11/20241015114548-QIM5KDRH.json"
+                        title="AI Chatbot"
+                        className="chatbot-frame"
+                        allow="microphone"
+                    ></iframe>
+                </div>
+            )}
         </div>
     );
 }
