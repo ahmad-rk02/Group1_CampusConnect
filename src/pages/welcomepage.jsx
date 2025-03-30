b1ac0a93a61e317f8b11294ebed7470a1ec81f28
ahmad-rk02 <razakhanahmad68@gmail.com>
2025-03-30 15:42:17 +0530

    added

--- src/pages/welcomepage.jsx
+++ src/pages/welcomepage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
+import './welcomepage.css'
 
 function WelcomeSection() {
   const [keyPeople, setKeyPeople] = useState([]);
