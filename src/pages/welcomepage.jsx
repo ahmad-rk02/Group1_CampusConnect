9f1d1a622cad6491b256a32cc6d99c99f80fc258
ahmad-rk02 <razakhanahmad68@gmail.com>
2025-03-30 15:38:46 +0530

    added

--- src/pages/welcomepage.jsx
+++ src/pages/welcomepage.jsx
@@ -13,7 +13,7 @@ function WelcomeSection() {
             id: person.id,
             name: person.name,
             designation: person.designation,
-            imageUrl: person.image?.data?.url || ''
+            imageUrl: person.image?.url ? `${import.meta.env.VITE_STRAPI_MEDIA_BASE_URL}${person.image.url}` : ''
           })));
         }
       })
@@ -23,7 +23,6 @@ function WelcomeSection() {
   return (
     <Container fluid className="my-5 px-3 px-md-5 welcome-section">
       <Row className="align-items-start">
-        {/* Welcome Section */}
         <Col xs={12} md={8} className="mb-4 wel">
           <h3 className="fw-bold text-center text-md-start">
             Welcome to{' '}
@@ -32,21 +31,18 @@ function WelcomeSection() {
             </span>
           </h3>
           <p className="welcome-para">
-            Government College of Engineering Chandrapur is established in 1996 This is the only Government Institute under Gondwana University, Gadchiroli. This Government institute is completely funded by Government of Maharashtra. The Institute is under Director of Technical Education, M.S., Mumbai and is administered through its Regional office at Nagpur. Now a days, due to globalization there is stiff competition at the national & International level as well phenomenal growth in the technology. For this, competent technocrats & engineers are in great demand and to serve this requirement, Government College of Engineering,Chandrapur is taking efforts to produce high quality technocrats.
+            Government College of Engineering Chandrapur is established in 1996. This is the only Government Institute under Gondwana University, Gadchiroli. This Government institute is completely funded by Government of Maharashtra. The Institute is under Director of Technical Education, M.S., Mumbai and is administered through its Regional office at Nagpur. Nowadays, due to globalization, there is stiff competition at the national & international level as well as phenomenal growth in technology. For this, competent technocrats & engineers are in great demand, and to serve this requirement, Government College of Engineering, Chandrapur is taking efforts to produce high-quality technocrats.
           </p>
         </Col>
 
-        {/* Key People Section */}
         <Col xs={12} md={4} className="key-people-section">
           <h5 className="fw-bold text-center text-md-start">Key People</h5>
-
-          {/* Dynamically Render Key People */}
           {keyPeople.map((person) => (
             <Card key={person.id} className="mb-3 border-0">
               <Row className="g-0 align-items-center">
                 <Col xs={4} md={4} className="text-center">
                   <Card.Img
-                    src={`${import.meta.env.VITE_STRAPI_MEDIA_BASE_URL}${person.imageUrl}`}
+                    src={person.imageUrl}
                     alt={person.name}
                     className="img-fluid rounded-circle"
                   />
